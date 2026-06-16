a0933e63ce4ad5687d2f244f023a3c782d9d8d98
Mark Ward <mark.ward@gmail.com>
2026-06-16 07:57:08 +0100

    feat(renderer): request OpenGL 4.1 core context for tessellation

--- native/src/renderer/window.cc
+++ native/src/renderer/window.cc
@@ -37,8 +37,8 @@ void release_glfw() {
 Window::Window(int width, int height, const std::string& title, bool visible) {
     ensure_glfw();
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_VISIBLE, visible ? GLFW_TRUE : GLFW_FALSE);
